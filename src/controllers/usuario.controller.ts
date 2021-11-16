bb3e04a958b91e21c9970ea3bb90b754999c5921
Nicolas <bernalmedina.n@gmail.com>
2021-11-16 13:34:04 -0500

    Autenticacion por correo

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,29 +1,29 @@
+import {service} from '@loopback/core';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
+import fetch from "node-fetch";
 import {Usuario} from '../models';
 import {UsuarioRepository} from '../repositories';
+import {AutenticacionService} from '../services';
+//const fetch = require ('node-fetch');
 
 export class UsuarioController {
   constructor(
     @repository(UsuarioRepository)
     public usuarioRepository : UsuarioRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion: AutenticacionService
   ) {}
 
   @post('/usuarios')
@@ -44,7 +44,21 @@ export class UsuarioController {
     })
     usuario: Omit<Usuario, 'id'>,
   ): Promise<Usuario> {
-    return this.usuarioRepository.create(usuario);
+
+    let clave = this.servicioAutenticacion.GenerarClave();
+    let claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    let u = await this.usuarioRepository.create(usuario);
+    usuario.Clave = claveCifrada;
+
+    //Notificar Usuario usadno spider-flask
+    let destino = usuario.CorreoElectronico;
+    let asunto = 'Registro en la plataforma';
+    let contenido = `Hola ${usuario.Nombre}, su usuario es: ${usuario.CorreoElectronico}, y su contraseña será ${clave}`;
+    fetch(`http://127.0.0.1:5000/envio-correo?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+      .then((data:any) => {
+        console.log(data);
+      })
+      return u;
   }
 
   @get('/usuarios/count')
