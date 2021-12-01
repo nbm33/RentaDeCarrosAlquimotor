c00a7f42c093601f47dfec500a8652eb39158ca2
Nicolas <bernalmedina.n@gmail.com>
2021-12-01 10:53:25 -0500

    Crypto funcionado

--- src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators} from '@angular/forms';
 import { SeguridadService } from 'src/app/servicios/seguridad.service';
-const cryptoJS = require('cryptojs');
+import * as CryptoJS from 'crypto-js';
+import { ModeloIdentificar } from 'src/app/modelos/identificar.modelo';
 
 @Component({
   selector: 'app-identificacion',
@@ -20,13 +21,14 @@ export class IdentificacionComponent {
     console.log(this.fbValitador.value);
     let usuario = this.fbValitador.controls['usuario'].value;
     let clave =  this.fbValitador.controls['clave'].value;
-    let claveCifrada = cryptoJS.MD5(clave);
-    this.servicioSeguridad.identificar(usuario, claveCifrada).subscribe((datos:any) => {
+    let claveCifrada = CryptoJS.MD5(clave).toString();
+    this.servicioSeguridad.identificar(usuario,claveCifrada).subscribe((datos:any) => {
+      this.servicioSeguridad.almacenarSesion(datos);
       alert("Ingreso correcto")
     }, (error:any) => {
+      //KO
       alert("Datos invalidos")
     }
-    
     )
   }
 }
--- src/app/servicios/seguridad.service.ts
+++ src/app/servicios/seguridad.service.ts
@@ -24,4 +24,8 @@ export class SeguridadService {
     })
   }
 
+  almacenarSesion(datos: ModeloIdentificar) {
+
+  };
+  
 }
