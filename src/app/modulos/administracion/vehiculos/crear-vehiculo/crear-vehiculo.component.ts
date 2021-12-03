c70b5069c55dbed1a76d479c1b317e2e1e3871b8
Nicolas <bernalmedina.n@gmail.com>
2021-12-02 22:32:49 -0500

    validacion rol y crear vehiculo

--- src/app/modulos/administracion/vehiculos/crear-vehiculo/crear-vehiculo.component.ts
+++ src/app/modulos/administracion/vehiculos/crear-vehiculo/crear-vehiculo.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
 
 @Component({
   selector: 'app-crear-vehiculo',
   templateUrl: './crear-vehiculo.component.html',
   styleUrls: ['./crear-vehiculo.component.css']
 })
-export class CrearVehiculoComponent implements OnInit {
+export class CrearVehiculoComponent {
+  fbValitador = this.fb.group({
+    imagen: ['', [Validators.required]],
+    tipo: ['', [Validators.required]],
+    marca: ['', [Validators.required]],
+    modelo: ['', [Validators.required]],
+    des: ['', [Validators.required]],
+    valor: ['', [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private servicioSeguridad: SeguridadService, private router: Router) { }
 
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.fbValitador.value);
+    let imagen = this.fbValitador.controls['imagen'].value;
+    let tipo =  this.fbValitador.controls['tipo'].value;
+    let marca =  this.fbValitador.controls['marca'].value;
+    let modelo =  this.fbValitador.controls['modelo'].value;
+    let des =  this.fbValitador.controls['des'].value;
+    let valor =  this.fbValitador.controls['valor'].value;
+    alert("Administrador: Su registro de vehiculo fue aprobado")
+    // this.servicioSeguridad.Registrar(nombre,apellido,correo,celular,rol).subscribe((datos:any) => {
+    //   this.servicioSeguridad.AlmacenarUsuario(datos);
+    //   alert("Alquimotor: Registro Valido, le llegara un su correo electrónico la clave. Por revise, y vualva a esta plataforma y podra ingresar usando el boton de Inisio Sesion");
+    //   this.router.navigate(["/inicio"])
+    // }, (error:any) => {
+    //   //KO
+    //   alert("Alquimotor: Registro invalidos")
+    // }
+    // )
   }
 
 }
