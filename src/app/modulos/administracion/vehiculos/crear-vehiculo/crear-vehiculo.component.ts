5cac9c76835ed74e2a9db260d28ab2929651e717
Nicolas <bernalmedina.n@gmail.com>
2021-12-10 17:51:07 -0500

    Antes de merge trb Andrea

--- src/app/modulos/administracion/vehiculos/crear-vehiculo/crear-vehiculo.component.ts
+++ src/app/modulos/administracion/vehiculos/crear-vehiculo/crear-vehiculo.component.ts
@@ -1,7 +1,9 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ModeloPuntoAlquiler } from 'src/app/modelos/puntoAlquiler.modelo';
 import { ModeloVehiculo } from 'src/app/modelos/vehicolo.modelo';
+import { PuntoAlquilerService } from 'src/app/servicios/punto-alquiler.service';
 import { VehiculosService } from 'src/app/servicios/vehiculos.service';
 
 @Component({
@@ -9,7 +11,8 @@ import { VehiculosService } from 'src/app/servicios/vehiculos.service';
   templateUrl: './crear-vehiculo.component.html',
   styleUrls: ['./crear-vehiculo.component.css']
 })
-export class CrearVehiculoComponent {
+export class CrearVehiculoComponent implements OnInit {
+  
   fbValitador = this.fb.group({
     imagen: ['', [Validators.required]],
     tipo: ['', [Validators.required]],
@@ -17,9 +20,22 @@ export class CrearVehiculoComponent {
     modelo: ['', [Validators.required]],
     des: ['', [Validators.required]],
     valor: ['', [Validators.required]],
+    punto: [''],
   });
 
-  constructor(private fb: FormBuilder, private vehiculoService: VehiculosService, private router: Router) { }
+  listadoPuntos: ModeloPuntoAlquiler[] = [];
+
+  constructor(private fb: FormBuilder, private vehiculoService: VehiculosService, private puntoService: PuntoAlquilerService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.ObtenerListaPuntos();
+  }
+
+  ObtenerListaPuntos(){
+    this.puntoService.ObtenerPuntos().subscribe((datos: ModeloPuntoAlquiler[]) => {
+      this.listadoPuntos = datos;
+    })
+  }
 
   onSubmit() {
     console.log(this.fbValitador.value);
@@ -29,6 +45,7 @@ export class CrearVehiculoComponent {
     let modelo =  parseInt(this.fbValitador.controls['modelo'].value);
     let des =  this.fbValitador.controls['des'].value;
     let valor =  parseInt(this.fbValitador.controls['valor'].value);
+    let punto =  this.fbValitador.controls['punto'].value;
     let v = new ModeloVehiculo();
     v.imagen = imagen;
     v.Tipo = tipo;
@@ -39,7 +56,7 @@ export class CrearVehiculoComponent {
     v.Disponibilidad = true;
     v.administradorId = "";
     v.asesoresId = "";
-    v.puntoAlquilerId = "";
+    v.puntoAlquilerId = punto;
 
     this.vehiculoService.CrearVehiculos(v).subscribe((datos: ModeloVehiculo) => {
       alert("Alquimotor: Registro de vehiculo valido");
